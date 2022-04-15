3ce27c0172ce889aeb7b357d56c4436e9e6ac894
Emrullah <emr.ozk.71@gmail.com>
2022-04-16 00:27:55 +0300

    first commit

---
+++ src/test/java/com/example/educationapp/EducationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
