cf5c8545c340eea042b087cba5fe2d138d4f545b
Emrullah <emr.ozk.71@gmail.com>
2022-04-25 01:21:20 +0300

    Some configurations added.

--- src/main/java/com/example/educationapp/EducationAppApplication.java
+++ src/main/java/com/example/educationapp/EducationAppApplication.java
@@ -2,8 +2,10 @@ package com.example.educationapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EducationAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/educationapp/rest/UserRest.java
+++ src/main/java/com/example/educationapp/rest/UserRest.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin("*")
 public class UserRest {
 
     UserRepository userRepository;
